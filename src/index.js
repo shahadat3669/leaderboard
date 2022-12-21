4fc49fef555f6328087eac9b579c20f9bf98ea83
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-21 23:27:55 +0600

    Add eventListener for addScoreForm to send data to API.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@ import { addDataToApi, generateLeaderBoard } from './modules/leaderboard.js';
 import Score from './modules/Score.js';
 
 const scoreList = document.querySelector('#scoreList');
+const addScoreForm = document.querySelector('#addScoreForm');
+
+addScoreForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const newScore = new Score({
+    user: data.get('user'),
+    score: data.get('score'),
+  });
+  addDataToApi(newScore);
+});
+
 window.addEventListener('DOMContentLoaded', generateLeaderBoard(scoreList));
