f532ba07f2f02132d96cabc78b2e3d34c93e27b4
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-21 23:20:04 +0600

    Add getData method to get score List.

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,2 +1,12 @@
 const gameId = 'VMhOOqVVQvAkTf1NZgzA';
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
+
+const getData = async () => {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  return response.json();
+};
+
+export { getData };
