c3b3246d6bbf14d117d084d1e9c4a15a7602cf68
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-21 23:27:09 +0600

    Use window eventListener to generate score list from api when refresh the page.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './style.scss';
 import './assets/logo.png';
 import { addDataToApi, generateLeaderBoard } from './modules/leaderboard.js';
 import Score from './modules/Score.js';
+
+const scoreList = document.querySelector('#scoreList');
+window.addEventListener('DOMContentLoaded', generateLeaderBoard(scoreList));
